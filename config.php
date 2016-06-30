6e92fd3698e4ff57d8820f4ae5a6f50c34e10b76
Calvin Secrest <csecrest2@gmail.com>
2016-06-29 20:32:58 -0400

    This is an update
    This is the update for my configuration php

--- config.php
+++ config.php
@@ -1,13 +1,20 @@
 <?php
 
-	//database settings
+//database settings
 
-	$hostname = 'localhost';
-	$username = 'yourusername';
-	$password = 'yourpassword';
-	$dbname = 'poll';
+$servername = "localhost";
+$username = "username";
+$password = "password";
 
-	$connect = mysql_connect($hostname, $username, $password);
-	mysql_select_db($dbname);
-
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+    
 ?>
